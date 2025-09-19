4a879889a769b8cbb9f75c07627bd28d3fa7bcda
Krisha <krishatuladhar@gmail.com>
2025-09-19 17:46:18 +0545

    setup project configuration and base router

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
+
+// Create Express app
+const app = express();
+
+// Connect to database
+await connectDB()
+
+// Middleware
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => res.send("Welcome to Learnify API"));
+app.post('/clerk', express.json() , clerkWebhooks)
+
+// Start server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
