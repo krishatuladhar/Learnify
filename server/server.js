cd7b15f139b4889d1865ad0c09e48161c6396e63
Krisha <krishatuladhar@gmail.com>
2025-09-27 17:34:23 +0545

    Educator controller completed

--- server/server.js
+++ server/server.js
@@ -3,20 +3,29 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./config/cloudinary.js";
+import { getEducatorCourses } from "./controllers/educatorController.js";
 
 dotenv.config();
 // Create Express app
 const app = express();
 
 // Connect to database
-await connectDB()
+await connectDB();
+await connectCloudinary();
 
 // Middleware
+app.use(express.json()); 
 app.use(cors());
+app.use(clerkMiddleware());
 
 // Routes
 app.get("/", (req, res) => res.send("Welcome to Learnify API"));
-app.post('/clerk', express.json() , clerkWebhooks)
+app.post('/clerk', clerkWebhooks);
+app.use('/api/educator', educatorRouter);
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
